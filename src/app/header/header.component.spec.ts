3e6dfa52f54e1314b3387cf813993a7a6f15837a
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-05-12 23:08:46 -0600

    basic-setup

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TemplateComponent } from './template.component';
+import { HeaderComponent } from './header.component';
 
-describe('TemplateComponent', () => {
-  let component: TemplateComponent;
-  let fixture: ComponentFixture<TemplateComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TemplateComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TemplateComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe-model';
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+recipes: Recipe[] = [new Recipe('Test', 'describing the recipe', 'http://contribute.geeksforgeeks.org/wp-content/uploads/geek.png')];
+  
+constructor() { }
+
+  ngOnInit() {
+  }
+
+}
