fa25a8e12f64e9604c6ad70351fb734dfae9208c
Saloni <saloniskalsekar@gmail.com>
2020-02-09 13:54:06 -0500

    dataBinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'Not yet created';
+  serverName = ' ';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
    }
 
+   onCreateServer() {
+     this.serverCreationStatus = 'Server was created now';
+   }
+   updateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
   ngOnInit(): void {
   }
 
