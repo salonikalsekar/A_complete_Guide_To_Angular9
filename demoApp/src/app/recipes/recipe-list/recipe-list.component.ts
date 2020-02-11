56122f4b41f1fe6a6d10d2689c350e75205e14e3
Saloni <saloniskalsekar@gmail.com>
2020-02-10 22:33:29 -0500

    setUp models

--- demoApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ demoApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('A test', 'This is a test', 'https://www.themediterraneandish.com/wp-content/uploads/2019/04/Italian-Baked-Chicken-Recipe-6.jpg'),
+    new Recipe('A test', 'This is a test', 'https://www.themediterraneandish.com/wp-content/uploads/2019/04/Italian-Baked-Chicken-Recipe-6.jpg')
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
